be4873b4da38c1c86375c5f8b9fc77fae16ee1c4
Veronica Lobkina <you@example.com>
2022-07-02 12:15:17 -0700

    deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -7,3 +7,13 @@ MechaCar_lm = lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_
 #determine p-value and r-squared value and save  in GitHub repository
 summary(MechaCar_lm)
 
+#Deliverable 2
+Suspension_Coil <- read.csv('Suspension_Coil.csv',check.names = F,stringsAsFactors = F)
+#using summarize function
+total_summary <- Suspension_Coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
+#create dataframe based on Manufacturing_lot by using group_by function
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),
+                                                                             Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
+
+
+
