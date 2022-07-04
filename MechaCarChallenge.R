16dc3ec39915e78a10753a7e47bc8dbe5afc6694
Veronica Lobkina <95447175+verosfbay@users.noreply.github.com>
2022-07-03 19:14:05 -0600

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,26 +1,46 @@
 #Deliverable 1
 library(dplyr)
-MechaCar <- read.csv('MechaCar_mpg.csv',check.names = F,stringsAsFactors = F)
-head(MechaCar)
-#perform linear regression using lm function
-MechaCar_lm = lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data =MechaCar)
-#determine p-value and r-squared value and save  in GitHub repository
-summary(MechaCar_lm)
 
-#Deliverable 2
-Suspension_Coil <- read.csv('Suspension_Coil.csv',check.names = F,stringsAsFactors = F)
-#using summarize function
-total_summary <- Suspension_Coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
-#create dataframe based on Manufacturing_lot by using group_by function
-lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),
-                                                                             Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
-#Deliverable 3
-#perform t-test for all Lots
-t.test(Suspension_Coil$PSI, mu=1500)
-# Perform t-test on Lot 1
-t.test(subset(Suspension_Coil,Manufacturing_Lot=="Lot1")$PSI,mu = 1500)
-# Perform t-test on Lot 2
-t.test(subset(Suspension_Coil,Manufacturing_Lot=="Lot2")$PSI,mu = 1500)
-# Perform t-test on Lot 3
-t.test(subset(Suspension_Coil,Manufacturing_Lot=="Lot3")$PSI,mu = 1500)
+#import mechcar.csv as a dataframe 
+library(tidyverse)
+mecha_mpg = read.csv(file = "./MechaCar_mpg.csv", check.names = F, stringsAsFactors = F)
 
+#Linear regression lm()
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_mpg)
+
+#Summary function
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_mpg)) 
+
+
+#Deliverable 2 
+
+# import suspension.csv file
+mecha_coil <- read.csv(file='./Suspension_Coil.csv',check.names=F,stringsAsFactors = F) 
+
+# create total summary dataframe using summarize()
+total_summary <- mecha_coil %>% summarize(Mean_PSI=mean(PSI),
+                                          Median_PSI=median(PSI),
+                                          Std_Dev_PSI=sd(PSI),
+                                          Var_PSI=var(PSI),
+                                          Num_Coil=n(), .groups = 'keep') 
+# summary using grouby()
+lot_summary <- mecha_coil  %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI),
+                                                                         Median_PSI=median(PSI),
+                                                                         Std_Dev_PSI=sd(PSI),
+                                                                         Var_PSI=var(PSI),
+                                                                         Num_Coil=n(), .groups = 'keep') 
+#Deliverable 3 
+
+#use t-test function to determine PSI acrsoos all manufacturing lots
+t.test(mecha_coil$PSI,mu=1500)
+
+# write <3 scrits using t.test and its subset argument to determine if the PSI is statistcally different. 
+#write on for all 3 lots
+
+lot1 <- subset(mecha_coil, Manufacturing_Lot=="Lot1")
+lot2 <- subset(mecha_coil, Manufacturing_Lot=="Lot2")
+lot3 <- subset(mecha_coil, Manufacturing_Lot=="Lot3")
+
+t.test(lot1$PSI,mu=1500)
+t.test(lot2$PSI,mu=1500)
+t.test(lot3$PSI,mu=1500)
